25178d5dba532f80b4866d24bcccfce74bcd95fe
Duda Zanotto <mariaeduardaaz1307@gmail.com>
2023-08-30 18:58:05 -0300

    atualizando aula

--- mod_produto/ProdutoDAO.py
+++ mod_produto/ProdutoDAO.py
@@ -1,18 +1,24 @@
 from fastapi import APIRouter
+from mod_produto.Produto import Produto
 router = APIRouter()
-# Criar os endpoints de produto: GET, POST, PUT, DELETE
-@router.get("/produto/", tags=["Produto"])
+# Criar as rotas/endpoints: GET, POST, PUT, DELETE
+
+@router.get("/produto/", tags=["Produtos"])
 def get_produto():
-  return {"msg": "get todos executado"}, 200
-@router.get("/produto/{id}", tags=["Produto"])
-def get_produto(id: int):
-  return {"msg": "get um executado"}, 200
-@router.post("/produto/", tags=["Produto"])
+    return {"msg": "get todos executado"}, 200
+
+@router.get("/produto/{id}", tags=["Produtos"])
+def get_produto(id: int, p: Produto):
+    return {"msg": "get um executado", "id": id, "nome": p.nome, "descrição": p.descricao, "valor unitario": p.valor_unitario}, 200
+
+@router.post("/produto/", tags=["Produtos"])
 def post_produto():
-  return {"msg": "post executado"}, 200
-@router.put("/produto/{id}", tags=["Produto"])
+    return {"msg": "post executado"}, 200
+
+@router.put("/produto/{id}", tags=["Produtos"])
 def put_produto(id: int):
-  return {"msg": "put executado"}, 201
-@router.delete("/produto/{id}", tags=["Produto"])
+    return {"msg": "put executado"}, 201
+
+@router.delete("/produto/{id}", tags=["Produtos"])
 def delete_produto(id: int):
-  return {"msg": "delete executado"}, 201
+    return {"msg": "delete executado"}, 201
